183f83c75056bd084dfffa5b9309e2c876d7d574
Mohammad Abdullah Al Mamun <mohammad.mamun@bjitgroup.com>
2022-06-01 11:34:15 +0600

    Changed 35

--- src/main/java/com/tomgregory/entity/ThemeParkRide.java
+++ src/main/java/com/tomgregory/entity/ThemeParkRide.java
@@ -8,19 +8,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Getter
 @ToString
-@NoArgsConstructor
 public class ThemeParkRide {
   @Id
   @GeneratedValue(strategy=GenerationType.AUTO)
   private Long id;
-  @NotEmpty
   private String name;
-  @NotEmpty
   private String description;
   private int thrillFactor;
   private int vomitFactor;
@@ -31,5 +27,7 @@ public class ThemeParkRide {
     this.thrillFactor = thrillFactor;
     this.vomitFactor = vomitFactor;
   }
+
+  public ThemeParkRide(){}
 
 }
